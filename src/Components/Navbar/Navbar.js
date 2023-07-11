e065a8debe2f4466a487ee595ca750f710d5416a
lokes1212 <lokeshsharma3303@gmail.com>
2023-07-11 17:17:21 +0530

    added links on navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,68 +3,36 @@ import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <div>
-      <nav class="navbar navbar-expand-lg --bs-success-text-emphasis">
-        <div class="container-fluid">
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class=".text-dark">
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item  left_nav">
-                  <a class="nav-link active" aria-current="page" href="/">
-                    Home
-                  </a>
-                </li>
-                <li class="nav-item  left_nav">
-                  <a class="nav-link " href="/">
-                    About
-                  </a>
-                </li>
-                <li class="nav-item left_nav">
-                  <a class="nav-link" href="/">
-                    Services
-                  </a>
-                </li>
+ <header>
+  <nav class="navbar">
+    <ul>
+      <li><a href="/" >Home</a></li>
+      <li><a href="/" >About</a></li>
+      <li><a href="/" >Services</a></li>
+      <li class="Logo">
+        <img src="/" alt="Logo"/>
+      </li>
+         
+    
 
-                <li class="nav-item center_nav">
-                  <a class="nav-link" href="/">
-                    <img
-                      src="https://picsum.photos/200"
-                      alt="Logo"
-                      width="100"
-                      height="50"
-                    />
-                  </a>
-                </li>
+  
+     
+        <div class="right">
+      <li class="right-nav"><a href="/" >blog</a></li>
+      <li class="right-nav"><a href="/" >Contact</a></li>
+      </div>
+      {/* <div class="bt">
 
-                <li class="nav-item right_nav">
-                  <a class="nav-link " href="/">
-                    Blog
-                  </a>
-                </li>
-                <li class="nav-item right_nav">
-                  <a class="nav-link " href="/">
-                    Contact
-                  </a>
-                </li>
-                <li class="nav-item right_nav">
-                  <button class="nav-link ">Free Quote</button>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </div>
+<button>free Qoute</button>
+</div>
+      */}
+      </ul>
+      
+     
+   
+  </nav>
+ </header>
+  
+   
   );
 }
